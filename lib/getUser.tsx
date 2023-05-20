6981df6e2256d07f651aa180878973a73977766b
Pichborith Kong <pichborith@gmail.com>
2023-05-19 21:42:40 -0700

    make project to become SSG

--- lib/getUser.tsx
+++ lib/getUser.tsx
@@ -1,7 +1,9 @@
 async function getUser(userId: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${userId}`);
 
-  if (!res.ok) throw new Error('Failed to fetch user');
+  // if (!res.ok) throw new Error('Failed to fetch user');
+  // return undefined to let the build-in notFound of Next handle this
+  if (!res.ok) return undefined;
 
   return res.json();
 }
--- lib/getUserPosts.tsx
+++ lib/getUserPosts.tsx
@@ -1,9 +1,12 @@
 async function getUserPosts(userId: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/posts?userId=${userId}`
+    `${process.env.NEXT_PUBLIC_API_URL}/posts?userId=${userId}`,
+    { next: { revalidate: 60 } }
   );
 
-  if (!res.ok) throw new Error('Failed to fetch user');
+  // if (!res.ok) throw new Error('Failed to fetch user');
+  // return undefined to let the build-in notFound of Next handle this
+  if (!res.ok) return undefined;
 
   return res.json();
 }
